62734199cf11bb780599aa228329c661dedfe694
szhangbiao <szhangbiao@gmail.com>
2025-12-17 10:23:48 +0800

    Fix local running issues

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,9 @@ export default defineConfig({
         emptyOutDir: true,
     },
     server: {
+        host: '127.0.0.1',
         port: 5173,
+        strictPort: true,
         proxy: {
             // 将 /api 请求代理到 Wrangler Pages Dev 服务器
             // 这样在开发时访问 http://localhost:5173 也能正常调用 API
